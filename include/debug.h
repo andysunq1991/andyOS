0be541aa88ceb0646a230a2ed6cc22ef408bea05
andyinsieyuan <andyinsieyuan@126.com>
2018-03-02 17:14:55 +0800

    完善string.c

--- include/debug.h
+++ include/debug.h
@@ -9,3 +9,11 @@
  *         Author: andysun
  * =====================================================================================
  */
+#include "console.h"
+#include "vargs.h"
+
+//内核的打印函数
+void printk(const char *format,...);
+
+//内核的打印函数带颜色
+void printk_color(real_color_t back, real_color_t fore, const char *format,...);
--- include/string.h
+++ include/string.h
@@ -9,3 +9,17 @@
  *         Author: andysun
  * =====================================================================================
  */
+#ifndef INCLUDE_STRING_H_
+#define INCLUDE_STRING_H_
+
+#include "types.h"
+
+void memcpy(uint8_t *dest, const uint8_t *src, uint32_t len);
+void memset(uint8_t *dest, uint8_t val, uint32_t len);
+void bzero(uint8_t *dest, uint32_t len);
+int strcmp(const char *str1, const char *str2);
+char *strcpy(char *dest, const char *src);
+char *strcat(char *dest, const char *src);
+int strlen(const char *src);
+
+#endif//end of INCLUDE_STRING_H_
