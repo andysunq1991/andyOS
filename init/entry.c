8928d0e38da7350b5406ba088315b7963345fb99
andyinsieyuan <andyinsieyuan@126.com>
2018-03-06 10:03:14 +0800

    $8完成初步编码

--- init/entry.c
+++ init/entry.c
@@ -18,10 +18,13 @@ int kern_entry()
 	init_debug();
 	init_gdt();
 	init_idt();
+
 	console_clear();
 	printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
 	asm volatile ("int $0x3");
 	asm volatile ("int $0x4");
+	init_timer(200);
+	asm volatile ("sli");
 	//panic("TEST");
 	return 0;
 }
